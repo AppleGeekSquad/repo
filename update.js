41f0126f413abdcb2b08e8292cd98342287cb8f8
AppleGeekSquad <appletecher03@gmail.com>
2018-03-28 20:53:12 -0400

    Update update.js

--- update.js
+++ update.js
@@ -18,7 +18,7 @@ function login(){
 	for(var i = 0;i<users.length;i++){
 		if(document.getElementById("user").value == users[i]){
 			if(document.getElementById("pass").value == passes[i]){
-				window.open("https://applegeeksquad.github.io/berlus.github.io/");	
+				window.open("https://applegeeksquad.github.io/berlus/sections/exploits.html");	
 			}
 		}
 	}
